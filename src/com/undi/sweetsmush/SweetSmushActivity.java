2fee1f5e3514e0602dcf6da135bef5e3140ab77f
Corey <corey@undiwork>
2013-09-09 10:14:27 -0400

    Set up activity to start with titleView

--- src/com/undi/sweetsmush/SweetSmushActivity.java
+++ src/com/undi/sweetsmush/SweetSmushActivity.java
@@ -1,15 +1,19 @@
 package com.undi.sweetsmush;
 
+import com.undi.sweetsmush.view.TitleView;
+
 import android.app.Activity;
 import android.os.Bundle;
 
 public class SweetSmushActivity extends Activity
 {
+	TitleView titleView;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+    	titleView = new TitleView(this);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(titleView);
     }
 }
